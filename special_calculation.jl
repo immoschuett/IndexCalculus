b1cbb6eb1991a5e042b0dcf709e8b6575b56a556
Immo <immo.schuett@outlook.de>
2022-01-17 17:23:46 +0100

    commit for 3 deletions

--- special_calculation.jl
+++ special_calculation.jl
@@ -11,14 +11,16 @@ function disc_logs(F,h)
     @debug issprime((p-1)/2) ? nothing : (@warn "(p-1)/2 not prime")
 
     #precalculate disc log relations
-    SP = sieve_params(p,0.02,1.2)
+    SP = sieve_params(p,0.02,1.2) #other ratio here for test
     RELMat,FB,FB_x,l = Sieve(F,SP)  #return Relation Matrix, Factorbase, extended Factorbase & length of Factorbase
 
     #find q_logs mod p-1/2
     modul = fmpz((p-1)/2)
     g_temp1 = F.a^2
     v = wiedemann(RELMat,modul)    # get v. s.t Relmat v = 0 
-    v = (invmod(lift(v[1]),modul)).*v #norm
+    
+    #v = (invmod(lift(v[1]),modul)).*v #norm
+    return v,get_logs(v,F,FB_x)
     FB_2,FB_x2,new_logs = new_base_with_logs(FField(GF(modul),g_temp1),FB_x,v,l)
     
     #find log_q mod 2 
@@ -63,6 +65,7 @@ function new_base_with_logs(F::FField,FB_x,v,l)
     return newFB_x[1:(sum(mask[1:l]))],FactorBase(newFB_x[1:(sum(mask[1:l]))]),logdict
 end
 
+#comment. we dont need CRT if we directly test F.a ^ log  F.a ^2 * log 
 
 function crs_fmpz(N::Array{fmpz},P::Array{fmpz},positive=true)
     product = fmpz(1)
@@ -83,15 +86,45 @@ end
 
 
 B = FField(GF(200087),primitive_elem(GF(200087),true))
-RELMat,FB,FB_x = Sieve(B, sieve_params(200087,0.02,1.1))
-h = B.a^(123)
-t = disc_logs(B,h)
+RELMat,FB,FB_x = Sieve(B, sieve_params(200087,0.02,1.2))
+rank(Array(RELMat))
+rank(Array(change_base_ring(ResidueRing(ZZ,Int(200086/2)),RELMat)))
+rank(Array(change_base_ring(ResidueRing(ZZ,Int(2)),RELMat)))
 
-println(t)
-#find q_logs mod p-1/2
-modul = fmpz((200087-1)/2)
+
+
+h = B.a^(123)
+t,s = disc_logs(B,h)
 
 v = wiedemann(RELMat,modul)
-
-
+brute_force(B,B.K(2)) 
+ 18778 /2 
 B.a^t
+
+function get_logs(v,F,FB) #FB_array
+    #Input v mod (p-1)/2
+    #Output logs of FB mod p-1
+    I = []
+    modulus = fmpz((length(F.K)-1)/2)
+    g = F.a 
+    for indx in 1:length(v) 
+        if g^lift(v[indx]) == FB[indx]
+            push!(I,v[indx])
+        elseif g^lift(2*v[indx]) == FB[indx]
+            push!(I,2*v[indx])
+        else 
+            push!(I,0)
+        end
+    end 
+    return I
+end
+
+        #later delete one check
+
+function brute_force(F,h)
+    for i in F.K 
+        if F.a^lift(i) == h 
+            return i 
+        end 
+    end 
+end 
