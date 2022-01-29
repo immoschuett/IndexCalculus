1fabc5997aa1f260d95e432aeb85484000729040
Berenike Dieterle <94169337+BD-00@users.noreply.github.com>
2022-01-29 01:17:12 +0100

    fixed, added til Prepro_5

--- preprocessing.jl
+++ preprocessing.jl
@@ -41,6 +41,7 @@ function sp_preprocessing_2(A, TA, l)
     n,m = A.r, TA.r
     done = false
     while !done 
+        done = true
         for j=l+1:m
             if length(TA[j]) == 2
                 done = false
@@ -53,14 +54,14 @@ function sp_preprocessing_2(A, TA, l)
                     add_scaled_row!(A, a, b, -divexact(w,v))
                     empty!(A[a].pos); empty!(A[a].values)
                     A.nnz-=length(A[a]) 
-                    @debug transpose(A)==TA ? nothing : (print(d,f))
+                    #@debug transpose(A)==TA ? nothing : (print(d,f))
                 else             #add B to A -> A = A - v/w*B
                     add_scaled_col_trans!(TA, A, b, a, -divexact(v,w))
                     delete_col(TA, A, b)
                     add_scaled_row!(A, b, a, -divexact(v,w))
                     empty!(A[b].pos); empty!(A[b].values)
                     A.nnz-=length(A[b])
-                    @debug transpose(A)==TA ? nothing : (print(d,f))
+                    #@debug transpose(A)==TA ? nothing : (print(d,f))
                 end 
             end
         end
@@ -75,10 +76,10 @@ end
 function sp_preprocessing_3(A, TA, l) #without comparison
     n,m = A.r, TA.r
     done = false
-    for counter = 1:10
-    #while !done
-        for j=1:m
-            if length(TA[j])==3
+    while !done
+        done = true
+        for j = 1:m
+            if length(TA[j]) == 3
                 done = false
                 (p,u),(q,v),(r,w) = sort([(TA[j].pos[i],TA[j].values[i]) for i=1:3], by=x->length(A[x[1]]), rev=true)
                 add_scaled_col_trans!(TA, A, p, q, -divexact(v,u)) #add P to Q -> Q = Q - v/u *P
@@ -98,15 +99,72 @@ function sp_preprocessing_3(A, TA, l) #without comparison
 end
 
 
+function sp_preprocessing_4(A, TA, l) #without comparison
+    n,m = A.r, TA.r
+    done = false
+    while !done
+        done = true
+        for j = 1:m
+            if length(TA[j]) == 4
+                done = false
+                (p,u),(q,v),(r,w),(s,x) = sort([(TA[j].pos[i],TA[j].values[i]) for i=1:4], by=x->length(A[x[1]]), rev=true)
+                add_scaled_col_trans!(TA, A, p, q, -divexact(v,u)) #add P to Q -> Q = Q - v/u *P
+                add_scaled_col_trans!(TA, A, p, r, -divexact(w,u)) #add P to R -> R = R - w/u *P
+                add_scaled_col_trans!(TA, A, p, s, -divexact(x,u)) #add P to S -> S = S - x/u *P
+                delete_col(TA, A, p)
+                add_scaled_row!(A, p, q, -divexact(v,u))
+                add_scaled_row!(A, p, r, -divexact(w,u))
+                add_scaled_row!(A, p, s, -divexact(x,u))
+                empty!(A[p].pos); empty!(A[p].values)
+                A.nnz-=length(A[p]) 
+            end
+        end 
+    end
+    A = delete_zero_rows(A)
+    TA = transpose(A)
+    A = transpose(delete_zero_rows(TA,l+1))      
+    return A, TA
+end
+
+
+function sp_preprocessing_5(A, TA, l) #without comparison
+    n,m = A.r, TA.r
+    done = false
+    while !done
+        done = true
+        for j = 1:m
+            if length(TA[j]) == 5
+                done = false
+                (p,u),(q,v),(r,w),(s,x),(t,y) = sort([(TA[j].pos[i],TA[j].values[i]) for i=1:5], by=x->length(A[x[1]]), rev=true)
+                add_scaled_col_trans!(TA, A, p, q, -divexact(v,u)) #add P to Q -> Q = Q - v/u *P
+                add_scaled_col_trans!(TA, A, p, r, -divexact(w,u)) #add P to R -> R = R - w/u *P
+                add_scaled_col_trans!(TA, A, p, s, -divexact(x,u)) #add P to S -> S = S - x/u *P
+                add_scaled_col_trans!(TA, A, p, t, -divexact(y,u)) #add P to T -> T = T - y/u *P
+                delete_col(TA, A, p)
+                add_scaled_row!(A, p, q, -divexact(v,u))
+                add_scaled_row!(A, p, r, -divexact(w,u))
+                add_scaled_row!(A, p, s, -divexact(x,u))
+                add_scaled_row!(A, p, t, -divexact(y,u))
+                empty!(A[p].pos); empty!(A[p].values)
+                A.nnz-=length(A[p]) 
+            end
+        end 
+    end
+    A = delete_zero_rows(A)
+    TA = transpose(A)
+    A = transpose(delete_zero_rows(TA,l+1))      
+    return A, TA
+end
+
+
  function sp_preprocessing_cases(A, l)
     TA = transpose(A)
     n,m = A.r, TA.r
     done = false
-    d = 0
     while !done 
         done = true
         for j = l+1:m
-            if length(TA[j]) == 1
+            if length(TA[j]) == 1 
                 done = false
                 i = TA[j].pos[1]
                 delete_col(A, TA, j)
@@ -119,14 +177,22 @@ end
                 end
             elseif length(TA[j]) == 2
                 done = false
-                a, b = TA[j].pos
-                v, w  = TA[j].values
-                add_scaled_col_trans!(TA, A, a, b, -divexact(w,v))
-                delete_col(TA, A, a)
-                add_scaled_row!(A, a, b, -divexact(w,v))
-                empty!(A[a].pos); empty!(A[a].values)
-                A.nnz-=length(A[a]) 
-                @assert transpose(A)==TA
+                a, b = TA[j].pos #indices of rows
+                v, w  = TA[j].values #A[a,j], A[b,j]
+                p = length(A[a]); q = length(A[b]) #number of entries in rows
+                if p  > q        #add A to B -> B = B - w/v *A
+                    add_scaled_col_trans!(TA, A, a, b, -divexact(w,v))
+                    delete_col(TA, A, a)
+                    add_scaled_row!(A, a, b, -divexact(w,v))
+                    empty!(A[a].pos); empty!(A[a].values)
+                    A.nnz-=length(A[a]) 
+                else             #add B to A -> A = A - v/w*B
+                    add_scaled_col_trans!(TA, A, b, a, -divexact(v,w))
+                    delete_col(TA, A, b)
+                    add_scaled_row!(A, b, a, -divexact(v,w))
+                    empty!(A[b].pos); empty!(A[b].values)
+                    A.nnz-=length(A[b])
+                end 
             end
         end
     end
@@ -151,7 +217,7 @@ using Markdown, Nemo
 include("Magma_sieve.jl")
 include("wiedemann.jl")
 #include("FB_logs.jl")
-p = cryptoprime(10)
+p = cryptoprime(25)
 TESTFIELD = BigFField(GF(p),primitive_elem(GF(p),true))
 SP = sieve_params(p,0.02,1.1)
 RELMat,FB,FBx,l = Sieve(TESTFIELD,SP)
@@ -159,12 +225,17 @@ p = length(TESTFIELD.K)
 modulus_ = fmpz((p-1)/2)
 RR = ResidueRing(ZZ,modulus_)
 A = change_base_ring(RR,RELMat)
-#preprocessing_cases(A, l)
-#TA = transpose(A)
+density(A)
 A, TA = sp_preprocessing_1(A, l)
-#B, TB = A, TA
+density(A)
 A, TA = sp_preprocessing_2(A, TA, l)
+density(A)
 A, TA = sp_preprocessing_3(A, TA, l)
+density(A)
+A, TA = sp_preprocessing_4(A, TA, l)
+density(A)
+A,TA = sp_preprocessing_5(A, TA, l)
+density(A)
 A, TA = sp_preprocessing_cases(A, l)
 
 entries(A, TA, l)
