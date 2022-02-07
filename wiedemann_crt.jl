4593cd8671e9d7753ec289bb1363023f81e6c4d7
Immo <immo.schuett@outlook.de>
2022-02-07 23:10:02 +0100

    working edition wiedemann_crt

--- wiedemann_crt.jl
+++ wiedemann_crt.jl
@@ -21,6 +21,7 @@ function wiedemann_var_crt(A::SMat{fmpz_mod}) #N::fmpz || N::Int64
 	N = modulus(RR)
 	T = fmpz
 	A = change_base_ring(ZZ, A)::SMat{T}
+	#A = mod_sym!(A,N)
 	(n,m) = nrows(A),ncols(A);
     ##########################################################################################################################################
 	@debug begin 
@@ -43,30 +44,46 @@ function wiedemann_var_crt(A::SMat{fmpz_mod}) #N::fmpz || N::Int64
     #Wiedemann sequence
     # solve A^tAx = y2 => x -y in kernel(A^tA) to avoid finding zero vec
 	##########################################################################################################################################
-	M = max(maximum(A),-minimum(A))
+	M = 2'maximum(abs,A)
+	println(M)
 	B = n*(m*M*N)^2
+	B = 2*n*m^2*N^4
 	tr = nthreads()
-	t = root(B,tr)
+	t = root(B,tr)  #WARNING size here
 	p_0 = next_prime(t)
 	P = [p_0]
 	while prod(P) < B
 		push!(P,next_prime(P[end]))
 	end 
 	t = length(P)
+	E_p = crt_env(P)
+	P2 = Int.(P) 
 	@debug t == nthreads() 
 	##########################################################################################################################################
-	ACSC = sparse(A)
-	TACSC = sparse(TA)
+	ACSC = Int.(sparse(A))
+	TACSC = Int.(sparse(TA))
 	y = mul(TA, mul(A,r))
 	seq[1] = dot(randlin,c) #randlin*c 	
-	Sol = zeros(fmpz,m,t)
-	Sol_prealloc = [storing_m for i=1:t] #TODO 
+	Sol = zeros(Int64,t,m)
+	Sol_2 = zeros(Int64,t,m)
+	#Sol_3 = zeros(fmpz,n,t)
+	Sol_3 = zeros(Int64,t,n)
 	for i = 2:2*n  #Wiedemann sequence
-		for i = 1:t
-			Sol[:,i] = LinearAlgebra.mul!(Sol[:,i],TACSC, LinearAlgebra.mul!(storing_n,ACSC,c)).%P[i]
+		#c = [c c c c]
+		c0 = deepcopy(c)
+		#Sol = LinearAlgebra.mul!(Sol,TACSC, LinearAlgebra.mul!(Sol_3,ACSC,c))
+		#Sol = transpose(Sol).%P
+		#Sol = transpose(Sol)
+		c = transpose([c c c c])  # TODO to t times h or c-cat 
+		#inplace_conv2int(Sol_2,Sol).%P2
+		Sol = Int.(c.%P2)
+		Sol_2 = LinearAlgebra.mul!(Sol_2,LinearAlgebra.mul!(Sol_3,Sol,TACSC),ACSC) #TODO with Int64 inplace modular here
+		#Sol = Sol.%P2
 		#c = multact!(c,TA,(mulact!(storing,A,c,zero!(RR),st)),zero(RR),st) # generate sequence
-		end 
-		c = [ crt(Sol[i,:],P) for i in 1:m ].%N
+		Sol = fmpz.(Sol_2)
+		#Sol = inplace_conv2fmpz(Sol,Sol_2)
+		c = [ crt(Sol[:,i],E_p) for i in 1:m ].%N    # do this inplace here
+		#@debug c == mul(TA,mul(A,c0)) ? nothing : (@error "SEQ_wrong at $i" ,break)
 		seq[i] = dot(randlin,c)  #eleminates
 	end
     ##########################################################################################################################################
@@ -106,6 +123,15 @@ end
 # some multiplications:
 # somehow this does not work inplace, neither correct if c is inital zero???
 function multi!(c::Vector{fmpz}, A::SMat{fmpz}, b::Vector{fmpz})
+    t = fmpz()
+    for (i, r) in enumerate(A)
+      dot!(c[i],r,b,t)
+    end
+    @debug mul(A,b) == c || @error "MULT for fmpz fatal error"
+    return c
+end
+
+function mul_ptr!(c::Vector{fmpz}, A::SMat{fmpz}, b::Vector{fmpz})
     t = fmpz()
     for (i, r) in enumerate(A)
       dot!(c[i],r,b,t)
@@ -154,3 +180,148 @@ function dot!(s::fmpz_mod, sr::SRow{fmpz_mod}, a::Vector{fmpz_mod})
     return s
 end
 # some more in external test documents.
+
+#a = fpmz(4)
+#a.d 
+#typeof(a.d)
+# all boundet by 2^62 -1 if positiv (Int64 negative will result in 1 at 2nd bit) as negative Int64 architecture
+#mum prepro... ()  bounded by 2^ * ?  possible.
+using Hecke
+a = fmpz(100)
+while a.d == a 
+	a *=2
+end 
+c = 1000000000000000
+a = fmpz(4599999999999999999)
+a+=c
+b = fmpz(-4599999999999999999)
+b.d
+
+bitstring(2^62-1 + 2^62 + 2^63 +1 )
+2^65
+bitstring(2^62-1)
+
+@time a = fmpz(2^62-1)
+@time a.d
+@time a+=1
+@time a.d * a.d
+
+a = fmpz(100)
+@time a*a
+
+@time 100 * 100
+
+@time mul!(a.d,a.d,a.d)
+@time a.d *= 2^10
+
+a
+c = fmpz
+
+BigInt(2)^1000
+a = fmpz(100)
+b = fmpz(10)
+c = fmpz()
+function mul_ptr(c,a,b)
+	c.d = a*b.d 
+	return c 
+end 
+
+function mul_int(c,a,b)
+	c = a*b 
+	return c 
+end 
+
+@time  for i = 1:100000 mul_ptr(c,a,b) end 
+
+@time for i =  1:100000 1231231234%41 end
+
+function inplace_conv2fmpz(v::Vector{fmpz},w::Vector{Int64}) # if small
+	for i = 1:length(w)
+		v[i].d = w[i]
+	end 
+	return v
+end 
+function inplace_conv2fmpz(v::Matrix{fmpz},w::Matrix{Int64},size) # if small
+	for i = 1:size[1]
+		for j = 1:size[2]
+			v[i,j].d = w[i,j]
+			println(v[i,j])
+		end
+	end 
+	return v
+end 
+function inplace_conv2int(v::Vector{Int64},w::Vector{fmpz})  # if small
+	for i = 1:length(w)
+		v[i] = w[i].d
+	end 
+	return v
+end 
+function inplace_conv2int(v::Matrix{Int64},w::Matrix{fmpz})  # if small
+	for i = 1:length(w)
+		v[i] = w[i].d
+	end 
+	return v
+end 
+function inplace_mod_conv2int(v::Vector{Int64},w::Vector{fmpz},modulus::fmpz,z::fmpz) # if small
+	for i = 1:length(w)
+		zero!(z)
+		v[i] = mod!(z,w[i],modulus).d
+	end 
+	return v
+end 
+function inplace_modvec_conv2intmatrix(v::Matrix{Int64},w::Vector{fmpz},modulus::Vector{fmpz},z::fmpz) # if small
+	for j = 1:length(modulus)
+		for i = 1:length(w)
+			zero!(z)
+			@assert mod!(z,w[i],modulus[j]).d == mod!(z,w[i],modulus[j])
+			v[i,j] = mod!(z,w[i],modulus[j]).d
+		end 
+	end 
+	return v
+end 
+
+
+
+@time inplace_modvec_conv2intmatrix(TestMat,W,g,z)
+
+
+@time inplace_conv2int(V,W)
+
+V
+TestMat = zeros(Int,3,3)
+g = [fmpz(3),fmpz(2),fmpz(10)]
+z = fmpz()
+
+
+inplace_mod_conv2int(V,W,g,z)
+
+W = [fmpz(123) fmpz(122) fmpz(18); fmpz(2) fmpz(312) fmpz(1)]
+V = zeros(Int64,3)
+D = [1, 232, -4]
+Z = zeros(fmpz,3)
+@time K = inplace_conv2fmpz(Z,D)
+
+U  = zeros(Int64,4,4)
+V = zeros(fmpz,4,4)
+W = [1 2 3 4; 1 4 55 4; 1 4 555 5; 12 3 4 1]
+W = fmpz.(W)
+inplace_conv2int(U,W)
+
+
+V = inplace_conv2fmpz(V,W)
+[2, 3]
+A = [1 3;  3 4].%[2 ,3]
+
+V
+for i in W println(i) end
+
+W
+
+
+function inplace_conv2fmpz(v::Matrix{fmpz},w::Matrix{Int64}) # if small
+	for i = 1:length(W)
+		v[i].d = w[i]
+		println(v[i],w[i])
+	end
+ 	return v
+end 
